1f047ff5ee8afba99be837bbab8f01e11c4cd103
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-13 15:57:26 +0700

    ini namanye constructionbos tuh merahnya udah ada obatnya ternyata semicolon

--- inheritance.cpp
+++ inheritance.cpp
@@ -6,4 +6,12 @@ class orang
 {
     public:
     string nama;
-}
+
+    orang(string pNama) :nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
+
+     ~orang() {
+        cout << "Orang dihapus\n" << endl;
+    }
+};
