cc5e37b56404539a1e40edf8d29f5546cf780e05
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-13 16:06:21 +0700

    funggsi main dalamnya ada pmanggilan tapi di awalnya itu ada deklarasi lagipemberian nilai gituch  buat siswa1

--- inheritance.cpp
+++ inheritance.cpp
@@ -42,3 +42,14 @@ class pelajar : public orang
 
     }
 };
+
+
+nt main()
+{
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+
+}
