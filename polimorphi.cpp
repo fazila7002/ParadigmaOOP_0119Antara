d124e8141d6c204c56598025d041b97cb6190f44
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-13 16:26:16 +0700

    disini nihhh bikin polimorphi, pointer seseorang tinggal diganti alamatny nahh ntar pesan yg keluar ikut gantia

---
+++ polimorphi.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+    virtual void pesan() = 0;
+};
+
+class joko :public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari joko" << endl;
+    }
+};
+
+class lia :public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari lia" << endl;
+    }
+};
+
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    return 0;
+}
