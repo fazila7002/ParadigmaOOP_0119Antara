bdb1ad5d74611e84653b6b9655bf5815ad44ea65
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-13 16:04:17 +0700

    deklarasi again si string sekolah di kelas ini, trus ada constructorrrrrrr daan destructorrrr terusss di bawahnyaaa adaa fungsii perkenalannn (tak kenal maka tak sayang) azek

--- inheritance.cpp
+++ inheritance.cpp
@@ -23,5 +23,22 @@ class orang
 
 class pelajar : public orang
 {
+    string sekolah; 
 
+    pelajar(string pNama, string pSekolah) :orang(pNama), sekolah(pSekolah) {
+
+        cout << "Pelajar dibuat\n" << endl;
+
+    }
+   ~pelajar() {
+
+        cout << "Pelajar dihapus\n" << endl;
+
+    }
+
+    string perkenalan() {
+
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+
+    }
 };
